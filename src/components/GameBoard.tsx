dc5d1089a76449d19963843f800fdc65adaf80e4
ydev1992 <flyn.rider0x@gmail.com>
2024-09-30 10:58:36 -0400

    first game developing commit

---
+++ src/components/GameBoard.tsx
@@ -0,0 +1,349 @@
+import React, { useEffect, useState } from "react";
+import { AudioPlayer, AudioType } from "./gameComponents/audio";
+import { IBody, Body, StaticBody } from "./gameComponents/body";
+import { Drawer } from "./gameComponents/drawer";
+import { hasCollided } from "./gameComponents/collider";
+import {
+  BIRD,
+  BottomAxisBox,
+  CACTUS,
+  CAMERA,
+  CLOUD,
+  DAY_TIME_PERIOD,
+  DEBUG,
+  DINO,
+  GAME_SIZE,
+  HIGH_SCORE_BLINK,
+  LAND,
+  MOON,
+  STAR,
+} from "./gameComponents/config";
+import { InfoHandler, State } from "./gameComponents/info_handler";
+import { Key, KeyHandler } from "./gameComponents/key_handler";
+import { LandSpawner } from "./gameComponents/land";
+import { MoonMovement, MoonObj } from "./gameComponents/moon";
+import { IMovement, Movement } from "./gameComponents/movement";
+import { IObj, Obj } from "./gameComponents/obj";
+import {
+  Player,
+  PlayerBody,
+  PlayerMovement,
+  PlayerState,
+} from "./gameComponents/player";
+import { Spawner } from "./gameComponents/spawner";
+import { getNightDensity, randInt } from "./gameComponents/utils";
+
+const genCactusObj = (offX: number): IObj => {
+  const { types, pos, spd, acc } = CACTUS;
+  const { canvasType, colliderType } = types[randInt(0, types.length - 1)];
+  return new Obj(
+    (): IBody => {
+      return new StaticBody(canvasType, colliderType);
+    },
+    (): IMovement => {
+      return new Movement({ x: pos.x + offX, y: pos.y }, spd, acc);
+    }
+  );
+};
+
+const genBirdObj = (offX: number): IObj => {
+  const { frames, frameSeconds, pos, spd, acc } = BIRD;
+  return new Obj(
+    (): IBody => {
+      return new Body(frames, frameSeconds);
+    },
+    (): IMovement => {
+      return new Movement(
+        { x: pos.x + offX, y: pos.ys[randInt(0, pos.ys.length - 1)] },
+        { x: spd.x, y: spd.y },
+        acc
+      );
+    }
+  );
+};
+
+const genCloudObj = (offX: number): IObj => {
+  const { canvasType, colliderType, pos, spd, acc } = CLOUD;
+  return new Obj(
+    (): IBody => {
+      return new StaticBody(canvasType, colliderType);
+    },
+    (): IMovement => {
+      return new Movement(
+        { x: pos.x + offX, y: randInt(pos.y.min, pos.y.max) },
+        { x: randInt(spd.x.min, spd.x.max), y: spd.y },
+        acc
+      );
+    }
+  );
+};
+
+const genStarObj = (offX: number): IObj => {
+  const { frames, frameSeconds, pos, spd, acc } = STAR;
+  return new Obj(
+    (): IBody => {
+      return new Body(frames, frameSeconds);
+    },
+    (): IMovement => {
+      return new Movement(
+        { x: pos.x + offX, y: randInt(pos.y.min, pos.y.max) },
+        { x: spd.x, y: spd.y },
+        acc
+      );
+    }
+  );
+};
+
+const genLandObj = (offX: number): IObj => {
+  const { canvasType, colliderType, pos, spd, acc } = LAND;
+  return new Obj(
+    (): IBody => {
+      return new StaticBody(canvasType, colliderType);
+    },
+    (): IMovement => {
+      return new Movement({ x: pos.x + offX, y: pos.y }, spd, acc);
+    }
+  );
+};
+
+class Time {
+  private curTime: number = 0;
+
+  reset(): void {
+    this.curTime = 0;
+  }
+
+  update(timeElapsed: number): number {
+    this.curTime += timeElapsed;
+    return this.curTime;
+  }
+
+  getCurTime(): number {
+    return this.curTime;
+  }
+}
+
+function init() {
+  const time = new Time();
+  const camera = new Movement(CAMERA.pos, CAMERA.spd, CAMERA.acc);
+  const drawer = new Drawer(GAME_SIZE);
+  const starSpawner = new Spawner(STAR.minGap, STAR.maxGap, genStarObj);
+  const cactusSpawner = new Spawner(CACTUS.minGap, CACTUS.maxGap, genCactusObj);
+  const birdSpawner = new Spawner(BIRD.minGap, BIRD.maxGap, genBirdObj);
+  const cloudSpawner = new Spawner(CLOUD.minGap, CLOUD.maxGap, genCloudObj);
+  const landSpawner = new LandSpawner(genLandObj);
+  const moonObj = new MoonObj(
+    (): IBody => {
+      return new Body(MOON.frames, MOON.frameSeconds);
+    },
+    (): MoonMovement => {
+      return new MoonMovement(MOON.pos, MOON.spd, MOON.acc);
+    }
+  );
+  const player = new Player(
+    DINO.jumpSpd,
+    DINO.fallAcc,
+    DINO.hardDropAcc,
+    (): PlayerBody => {
+      const { frames, frameSeconds } = DINO.statusInfo[PlayerState.RUN];
+      return new PlayerBody(frames, frameSeconds);
+    },
+    (): PlayerMovement => {
+      return new PlayerMovement(DINO.pos, DINO.spd, DINO.acc);
+    }
+  );
+  const audioPlayer = new AudioPlayer();
+  const infoHandler = new InfoHandler(HIGH_SCORE_BLINK, GAME_SIZE);
+  const keyHandler = new KeyHandler();
+
+  const draw = () => {
+    const curTime = time.getCurTime();
+    const cameraPosX = camera.getPos().x;
+
+    drawer.clearCanvas();
+    const { isNight, nightDensity } = getNightDensity(curTime, DAY_TIME_PERIOD);
+    drawer.setCanvas(isNight, nightDensity);
+
+    if (DEBUG) {
+      cactusSpawner.drawColliderBoxes(drawer, cameraPosX);
+      birdSpawner.drawColliderBoxes(drawer, cameraPosX);
+      drawer.drawBoxes(
+        player.getColliderBoxes().map((box) => ({
+          left: box.left - cameraPosX,
+          top: box.top,
+          width: box.width,
+          height: box.height,
+        }))
+      );
+    }
+
+    if (isNight) {
+      starSpawner.drawCanvas(drawer, cameraPosX);
+      const { x, y } = moonObj.getPos();
+      drawer.drawCanvas(moonObj.getCanvasType(), { x: x - cameraPosX, y: y });
+    }
+
+    if (!infoHandler.checkIsState(State.NOT_START)) {
+      cloudSpawner.drawCanvas(drawer, cameraPosX);
+      landSpawner.drawCanvas(drawer, cameraPosX);
+      cactusSpawner.drawCanvas(drawer, cameraPosX);
+      birdSpawner.drawCanvas(drawer, cameraPosX);
+
+      infoHandler.drawCanvas(drawer);
+    }
+
+    const { x, y } = player.getPos();
+    drawer.drawCanvas(player.getCanvasType(), { x: x - cameraPosX, y: y });
+  };
+
+  const update = (timeElapsed: number) => {
+    const curTime = time.update(timeElapsed);
+
+    // camera movement
+    camera.update(curTime);
+    const cameraPosX = camera.getPos().x;
+
+    // objects movement
+    starSpawner.update(curTime, cameraPosX);
+    moonObj.updateMoon(curTime, cameraPosX);
+    cloudSpawner.update(curTime, cameraPosX);
+    landSpawner.update(curTime, cameraPosX);
+    cactusSpawner.update(curTime, cameraPosX);
+    birdSpawner.update(curTime, cameraPosX);
+    player.updatePlayer(curTime, keyHandler.getPressingKeys());
+    infoHandler.update(audioPlayer, cameraPosX);
+  };
+
+  const onKeyDown = (event: KeyboardEvent) => {
+    keyHandler.onKeyDown(event);
+    if (keyHandler.getPressingKeys().has(Key.ENTER)) {
+      if (infoHandler.checkIsState(State.GAME_OVER)) {
+        time.reset();
+        camera.reset();
+        starSpawner.reset();
+        cactusSpawner.reset();
+        birdSpawner.reset();
+        cloudSpawner.reset();
+        landSpawner.reset();
+        moonObj.reset();
+        player.reset();
+        infoHandler.reset();
+      }
+      infoHandler.updateState(State.PLAYING);
+    }
+    player.updateEvent(keyHandler.getPressingKeys());
+  };
+
+  const onKeyUp = (event: KeyboardEvent) => {
+    keyHandler.onKeyUp(event);
+    player.updateEvent(keyHandler.getPressingKeys());
+  };
+
+  const onResize = () => {
+    drawer.setCanvasSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+    draw();
+  };
+
+  return {
+    drawer,
+    cactusSpawner,
+    birdSpawner,
+    player,
+    audioPlayer,
+    infoHandler,
+    update,
+    draw,
+    onKeyDown,
+    onKeyUp,
+    onResize,
+  };
+}
+
+const {
+  drawer,
+  cactusSpawner,
+  birdSpawner,
+  player,
+  audioPlayer,
+  infoHandler,
+  update,
+  draw,
+  onKeyDown,
+  onKeyUp,
+  onResize,
+} = init();
+
+export default function GameBoard() {
+  const [gameStarted, setGameStarted] = useState(false);
+  const updatePeriod = 20;
+  useEffect(() => {
+    draw();
+
+    const interval = setInterval(() => {
+      if (infoHandler.checkIsState(State.PLAYING) && document.hasFocus()) {
+        setGameStarted(!infoHandler.checkIsState(State.NOT_START));
+        draw();
+        update(updatePeriod);
+
+        // check game status
+        if (
+          hasCollided(
+            player.getColliderBoxes(),
+            cactusSpawner.getColliderBoxes()
+          )
+        ) {
+          audioPlayer.playSound(AudioType.CRASH);
+          player.enterState(PlayerState.CRASH);
+          infoHandler.updateState(State.GAME_OVER);
+        }
+
+        if (
+          hasCollided(player.getColliderBoxes(), birdSpawner.getColliderBoxes())
+        ) {
+          if (player.getState() !== PlayerState.ATTACK) {
+            audioPlayer.playSound(AudioType.CRASH);
+            player.enterState(PlayerState.CRASH);
+            infoHandler.updateState(State.GAME_OVER);
+          } else {
+            birdSpawner.reset();
+          }
+        }
+      }
+    }, updatePeriod);
+
+    document.addEventListener("keydown", onKeyDown);
+    document.addEventListener("keyup", onKeyUp);
+    window.addEventListener("resize", onResize);
+
+    return () => {
+      clearInterval(interval);
+      document.removeEventListener("keydown", onKeyDown);
+      document.removeEventListener("keyup", onKeyUp);
+      window.addEventListener("resize", onResize);
+      infoHandler.saveHighScore();
+    };
+  }, []);
+
+  return (
+    <div>
+      <canvas
+        style={{ width: "100%" }} // no set height to follow canvas auto aspect ratio
+        width={window.innerWidth}
+        height={window.innerHeight}
+        ref={(ref) => ref && drawer.setCanvasRef(ref)}
+      />
+      {gameStarted === false ? (
+        <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <p className="text-white font-sans text-[24px]">
+            Press Enter to start Game
+          </p>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,178 @@
+import React, { useEffect, useState } from "react";
+import svg from "../assets/svg";
+import Img from "../assets/pepeImages";
+import tabList from "../assets/buttonList.png";
+import { Link } from "react-router-dom";
+import "../animation.scss";
+
+function LandingPage() {
+  const [currentImgIndex, setCurrentImgIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImgIndex((prevIndex) => (prevIndex + 1) % Img.length);
+    }, 1000); // 1000 milliseconds = 1 second
+
+    return () => clearInterval(intervalId);
+  }, []);
+  return (
+    <div className="w-full xl:min-h-screen flex flex-row justify-center bg-gradient-to-b from-[#000E1A] to-[#2D1D46]">
+      {/* container */}
+      <div className="w-full max-w-[1440px] relative pb-30 xl:h-[100vh]">
+        <div className="flex flex-col xl:flex-row xl:justify-between">
+          <div className="w-full px-[20px] xl:max-w-[50%] pt-20">
+            {/* title */}
+            <div className="mb-10 xl:mb-16" data-aos="fade-down">
+              <p
+                className="font-sans text-white text-[30px] xl:text-[40px] text-left neon-text rotating-text break-words"
+                data-text="play Pepe Trump Frog Game to collect money"
+              >
+                play Pepe Trump Frog Game to collect money
+              </p>
+            </div>
+
+            {/* token amount */}
+            <div
+              className="flex flex-row justify-center items-center relative mb-16 xl:mb-32"
+              data-aos="fade-up"
+              data-aos-duration="500"
+            >
+              <div className="rounded-[30px] border-[3px] border-[#02437B] bg-[414141] bg-opacity-[25] w-full p-1">
+                <p className="font-sans text-white text-[25px] xl:text-[40px]">
+                  <span className="text-[#FFDC00]">21365</span>
+                </p>
+              </div>
+              <div className="absolute left-[-10px] xl:left-[-20px] top-1/2 transform -translate-y-1/2 h-full flex items-center w-16 xl:w-full">
+                {svg.catCoinSVG}
+              </div>
+            </div>
+
+            {/* cat image */}
+            <div className="w-full xl:hidden relative" data-aos="zoom-in-up">
+              <div className="w-full rounded-[90px] overflow-hidden">
+                <div>
+                  <img src={Img[currentImgIndex]} className="w-full" />
+                </div>
+                <div className="absolute top-0 w-full h-full bg-[radial-gradient(circle_at_top,_var(--tw-gradient-stops))] from-[rgba(1,15,26,1)] via-[rgba(14,19,40,0.5)] to-[rgba(14,19,40,1)] text-white"></div>
+              </div>
+            </div>
+
+            {/* leader board */}
+            <div
+              className="relative"
+              data-aos="fade-right"
+              data-aos-offset="300"
+              data-aos-easing="ease-in-sine"
+            >
+              <div className="absolute left-1/2 transform -translate-y-1/2 -translate-x-1/2 rounded-[13px] border-[3px] bg-[#0B1225] p-[3px]">
+                <p className="font-sans text-[24px] xl:text-[32px] text-white p-1 xl:p-2">
+                  Leader-Board
+                </p>
+              </div>
+              <div className="border-[3px] rounded-[30px] border-[#BED9E6] bg-transparent sm:px-[10px] py-[40px] xl:p-[40px] w-full">
+                <div className="w-full flex flex-row gap-2 xl:gap-10 p-2 justify-center items-center">
+                  <div>{svg.firthMedal}</div>
+                  <div className="rounded-full border-2 border-white overflow-hidden w-[40px] h-[40px] bg-[url('./assets/avatar1.jpg')] bg-cover"></div>
+                  <div className="grow rounded-md border-transparent bg-[#434553] p-3 flex flex-row justify-between gap-10 font-jua text-white text-[17px] text-left">
+                    <p>Nguyen</p>
+                    <p className=" grow text-yellow-400 hidden xl:flex xl:flex-row xl:gap-2 xl:items-center">
+                      {svg.fullStar}
+                      {svg.fullStar}
+                      {svg.fullStar}
+                      {svg.fullStar}
+                      {svg.fullStar}
+                    </p>
+                    <p>488 714 827</p>
+                  </div>
+                </div>
+                <div className="w-full flex flex-row gap-2 xl:gap-10 p-2 justify-center items-center">
+                  <div>{svg.firthMedal}</div>
+                  <div className="rounded-full border-2 border-white overflow-hidden w-[40px] h-[40px] bg-[url('./assets/avatar1.jpg')] bg-cover"></div>
+                  <div className="grow rounded-md border-transparent bg-[#434553] p-3 flex flex-row justify-between gap-10 font-jua text-white text-[17px] text-left">
+                    <p>Nguyen</p>
+                    <p className=" grow text-yellow-400 hidden xl:flex xl:flex-row xl:gap-2 xl:items-center">
+                      {svg.fullStar}
+                      {svg.fullStar}
+                      {svg.fullStar}
+                      {svg.fullStar}
+                      {svg.fullStar}
+                    </p>
+                    <p>488 714 827</p>
+                  </div>
+                </div>
+                <div className="w-full flex flex-row gap-2 xl:gap-10 p-2 justify-center items-center">
+                  <div>{svg.firthMedal}</div>
+                  <div className="rounded-full border-2 border-white overflow-hidden w-[40px] h-[40px] bg-[url('./assets/avatar1.jpg')] bg-cover"></div>
+                  <div className="grow rounded-md border-transparent bg-[#434553] p-3 flex flex-row justify-between gap-10 font-jua text-white text-[17px] text-left">
+                    <p>Nguyen</p>
+                    <p className=" grow text-yellow-400 hidden xl:flex xl:flex-row xl:gap-2 xl:items-center">
+                      {svg.fullStar}
+                      {svg.fullStar}
+                      {svg.fullStar}
+                      {svg.fullStar}
+                      {svg.fullStar}
+                    </p>
+                    <p>488 714 827</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            className="w-full px-5 mt-5 xl:hidden"
+            data-aos="fade-up"
+            data-aos-anchor-placement="top-bottom"
+          >
+            <div className="w-full relative">
+              <div className="w-full">
+                <img src={tabList} className="w-full" />
+              </div>
+              <div className="absolute top-0 left-0 w-full flex flex-col grow pt-[10vw]">
+                <p className="font-bold text-[30px] font-sans mb-[10vw] menuBtn">
+                  Connect Wallet
+                </p>
+                <p className="font-bold text-[30px] font-sans mb-[10vw] menuBtn ">
+                  <Link to="/game">Start Game</Link>
+                </p>
+                <p className="font-bold text-[30px] font-sans menuBtn ">Shop</p>
+              </div>
+            </div>
+          </div>
+
+          {/* cat image */}
+          <div className="relative w-full max-w-[40%] h-screen hidden xl:block">
+            <div
+              className="absolute top-0 right-0 w-[576px] h-[576px] rounded-[167px] overflow-hidden hidden xl:block"
+              data-aos="zoom-in-up"
+            >
+              <div>
+                <img src={Img[currentImgIndex]} className="w-full" />
+              </div>
+              <div className="absolute top-0 w-full h-full bg-[radial-gradient(circle_at_top,_var(--tw-gradient-stops))] from-[rgba(1,15,26,1)] via-[rgba(14,19,40,0.5)] to-[rgba(14,19,40,1)] text-white"></div>
+            </div>
+            <div
+              className="absolute w-full xl:w-[540px] xl:h-[500px] bottom-0 xl:right-[20px] bg-[url('./assets/buttonList.png')] bg-cover"
+              data-aos="fade-up"
+              data-aos-anchor-placement="top-center"
+            >
+              <div className="flex flex-col grow pt-[65px]">
+                <p className="font-bold text-[40px] font-sans mb-[60px]  menuBtn">
+                  Connect Wallet
+                </p>
+                <p className="font-bold text-[40px] font-sans mb-[60px]  menuBtn">
+                  <Link to="/game">Start Game</Link>
+                </p>
+                <p className="font-bold text-[40px] font-sans menuBtn">Shop</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* footer */}
+      <div className="fixed w-full h-24 bottom-0 bg-[url('assets/footer.png')] bg-cover"></div>
+    </div>
+  );
+}
+export default LandingPage;
