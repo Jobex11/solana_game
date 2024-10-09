bff7f80757396b1412bef64f0ebacc989ab90efe
jobex11 <joblawal33@gmail.com>
2024-10-09 14:18:28 +0100

    made changes

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,9 @@ mongoose
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log(err));
 
-app.listen(3001, () => {
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
 
